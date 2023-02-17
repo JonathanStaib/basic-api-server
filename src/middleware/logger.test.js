6e80172349a270f0a09dcfd621207984122bcb0d
JonathanStaib <JonathanStaibb@gmail.com>
2023-02-17 03:09:37 +0000

    almost done with lab 03

--- src/middleware/logger.test.js
+++ src/middleware/logger.test.js
@@ -2,16 +2,17 @@
 
 const logger = require('./logger');
 
-describe ('Logger middleware', () => {
-  let req = {method: 'GET', path:'/favicon.ico'};
+describe('Logger middleware', () => {
+  let req = { method: 'GET', path: '/' };
   let res = {};
   let next = jest.fn();
   console.log = jest.fn();
-  
   it('logs method and path', () => {
     logger(req, res, next);
     let reqMethod = req.method;
     let path = req.path;
-    expect(console.log).toHaveBeenCalledWith(reqMethod, path);
+
+    expect(console.log).toHaveBeenCalledWith({ reqMethod, path });
+    expect(next).toHaveBeenCalledWith();
   });
 });
